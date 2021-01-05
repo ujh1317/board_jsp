1fedde8cbfbfcc734cebaf19e0fa163f729d6218
Yujeonghyeon <dbwjdgus1317@gmail.com>
2021-01-05 17:04:30 +0900

    01/05 UserDAO

---
+++ Board_jsp/src/user/User.java
@@ -0,0 +1,44 @@
+package user;
+// 자바빈즈 (회원정보를 담을 수 있는 유저테이블)
+public class User {
+
+	private String userID;
+	private String userPWD;
+	private String userName;
+	private String userGender;
+	private String userEmail;
+	
+	
+	public String getUserID() {
+		return userID;
+	}
+	public void setUserID(String userID) {
+		this.userID = userID;
+	}
+	public String getUserPWD() {
+		return userPWD;
+	}
+	public void setUserPWD(String userPWD) {
+		this.userPWD = userPWD;
+	}
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	public String getUserGender() {
+		return userGender;
+	}
+	public void setUserGender(String userGender) {
+		this.userGender = userGender;
+	}
+	public String getUserEmail() {
+		return userEmail;
+	}
+	public void setUserEmail(String userEmail) {
+		this.userEmail = userEmail;
+	}
+	
+	
+}//class
---
+++ Board_jsp/src/user/UserDAO.java
@@ -0,0 +1,48 @@
+package user;
+
+import java.sql.*;
+
+//데이터베이스 접근 객체
+public class UserDAO {
+
+	private Connection conn;
+	private PreparedStatement pstmt;
+	private ResultSet rs;
+	
+	public UserDAO(){
+		
+		try{ //DB접속
+			String URL = "jdbc:mysql://localhost:3306/bbs";
+			String USER = "root";
+			String PWD = "12345";
+			
+			Class.forName("com.mysql.jdbc.Driver");
+			
+			conn = DriverManager.getConnection(URL,USER,PWD);
+		}catch(Exception e){
+			e.printStackTrace(); //오류 출력
+		}//catch
+	}//UserDAO()
+	
+	public int login(String userID, String userPWD){
+		String sql = "select userPWD from user where userID = ?";
+		
+		try{ //로그인 시도
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setString(1, userID);
+			rs = pstmt.executeQuery();
+			if(rs.next()){
+				if(rs.getString(1).equals(userPWD))
+					return 1; //로그인 성공
+				else
+					return 0; //비밀번호 불일치
+			}//if
+			return -1; //아이디가 없을때
+		}catch(Exception e){
+			e.printStackTrace();
+		}//catch
+		return -2; //데이터베이스 오류
+	}//login()
+	
+	
+}//class
