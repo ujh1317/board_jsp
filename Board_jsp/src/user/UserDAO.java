a9aa67795eb64b00ad7161e06c2d61c406f3c4e9
Yujeonghyeon <dbwjdgus1317@gmail.com>
2021-01-08 18:12:09 +0900

    01/08 UserDAO - join()

--- Board_jsp/src/user/UserDAO.java
+++ Board_jsp/src/user/UserDAO.java
@@ -44,5 +44,21 @@ public class UserDAO {
 		return -2; //데이터베이스 오류
 	}//login()
 	
+	public int join(User user){
+		String sql = "insert into user values(?,?,?,?,?)";
+		try{
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setString(1, user.getUserID());
+			pstmt.setString(2, user.getUserPWD());
+			pstmt.setString(3, user.getUserName());
+			pstmt.setString(4, user.getUserGender());
+			pstmt.setString(5, user.getUserEmail());
+			return pstmt.executeUpdate();
+		}catch(Exception e){
+			e.printStackTrace();
+		}//catch
+		return -1; //데이터베이스 오류
+		
+	}//join()
 	
 }//class
